f1fb65db483d3b09fa20fcb38b9a78591a25a6ef
adityajoshi <aditya.reach@gmail.com>
2017-06-11 19:08:26 +0000

    shell script to add plugins in vim

---
+++ install_plugins.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git clone https://github.com/vim-airline/vim-airline.git && cp -rf ./vim-airline/* ~/.vim/
+git clone https://github.com/ctrlpvim/ctrlp.vim.git && cp -rf ./ctrlp.vim/* ~/.vim/
+git clone https://github.com/python-mode/python-mode.git && cp -rf ./python-mode/* ~/.vim/
