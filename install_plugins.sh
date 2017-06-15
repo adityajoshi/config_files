702caa154957dcc728698c1abf1f521feef1f86b
aditya joshi <aditya.reach@gmail.com>
2017-06-15 07:34:06 +0530

    Added git command to instal pathogen plugin

--- install_plugins.sh
+++ install_plugins.sh
@@ -3,3 +3,4 @@
 git clone https://github.com/vim-airline/vim-airline.git && cp -rf ./vim-airline/* ~/.vim/
 git clone https://github.com/ctrlpvim/ctrlp.vim.git && cp -rf ./ctrlp.vim/* ~/.vim/
 git clone https://github.com/python-mode/python-mode.git && cp -rf ./python-mode/* ~/.vim/
+git clone https://github.com/tpope/vim-pathogen.git && cp -rf ./vim-pathogen/* ~/.vim/
