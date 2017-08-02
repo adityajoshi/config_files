27adf662f8bba89380bb40bc527786072d2d7b69
aditya joshi <aditya.reach@gmail.com>
2017-08-02 07:53:14 +0530

    enabled vim for python3. also added "vim -version" output

---
+++ update_vim.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/aditya/code/vim
+git pull && ./configure --with-features=huge --enable-pythoninterp --enable-python3interp --enable-multibyte && make && sudo make install && make clean
+cp -rf ./runtime/* ~/.vim/.
