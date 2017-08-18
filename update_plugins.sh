feb8eadd022d169bdc8acec7782d1d1ae8e5a5a3
aditya joshi <aditya.reach@gmail.com>
2017-08-18 23:14:43 +0530

    Modified output path for plugins.

--- update_plugins.sh
+++ update_plugins.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd vim-airline && git pull && cd .. && cp -rf ./vim-airline/* ~/.vim/
-cd ctrlp.vim && git pull && cd .. && cp -rf ./ctrlp.vim/* ~/.vim/
-cd python-mode && git pull && cd .. && cp -rf ./python-mode/* ~/.vim/
-cd vim-pathogen && git pull && cd .. && cp -rf ./vim-pathogen/* ~/.vim/
+cd vim-airline && git pull && cd .. && cp -rf ./vim-airline ~/.vim/bundle/
+cd ctrlp.vim && git pull && cd .. && cp -rf ./ctrlp.vim ~/.vim/bundle/
+cd python-mode && git pull && cd .. && cp -rf ./python-mode ~/.vim/bundle/
+cd vim-pathogen && git pull && cd .. && cp -rf ./vim-pathogen/autoload/* ~/.vim/autoload/.
