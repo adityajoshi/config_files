705c1fe7f88682dfcc6de6ac2adbe48dae842de4
adityajoshi <aditya@adityajoshi.xyz>
2024-12-08 08:33:29 +0530

    remove file

--- update_plugins.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd vim-airline && git pull && cd .. && cp -rf ./vim-airline ~/.vim/bundle/
-cd ctrlp.vim && git pull && cd .. && cp -rf ./ctrlp.vim ~/.vim/bundle/
-cd python-mode && git pull && cd .. && cp -rf ./python-mode ~/.vim/bundle/
-cd vim-pathogen && git pull && cd .. && cp -rf ./vim-pathogen/autoload/* ~/.vim/autoload/.
